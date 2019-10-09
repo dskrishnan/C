a83149ec03005a21856f3558a6332a55112b4b1e
dskrishnan <16497990+dskrishnan@users.noreply.github.com>
2019-10-09 10:39:25 +0530

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by Santhanakrishnan Dhandapani on 10/9/19.
+//  Copyright © 2019 Santhanakrishnan Dhandapani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
